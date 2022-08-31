3889bbb8b94e9debab6aa10c887b680c988469b5
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-08-31 23:20:55 +0300

    Search Insert Position

---
+++ easy/Search Insert Position.js
@@ -0,0 +1,15 @@
+/*
+Search Insert Position
+
+https://leetcode.com/problems/search-insert-position/
+ */
+
+var searchInsert = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+
+    return nums.length;
+};
