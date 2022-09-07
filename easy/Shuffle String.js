8ca8e8194f71aad650ad7863dd8edd3738dd415a
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-09-07 15:30:57 +0300

    Shuffle String

---
+++ easy/Shuffle String.js
@@ -0,0 +1,15 @@
+/*
+Shuffle String
+
+https://leetcode.com/problems/shuffle-string/
+ */
+
+var restoreString = function (s, indices) {
+    const result = [];
+    for(let i = 0; i < s.length; i++) {
+        result[indices[i]] = s[i]
+    }
+    return result.join('');
+};
+
+console.log(restoreString('codeleet', [4, 5, 6, 7, 0, 2, 1, 3])) // leetcode
