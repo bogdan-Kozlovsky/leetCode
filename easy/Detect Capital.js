6176a17f7449df20195641db622e0fff30eabf10
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-09-14 20:13:40 +0300

    Detect Capital

---
+++ easy/Detect Capital.js
@@ -0,0 +1,21 @@
+/*
+Detect Capital
+
+https://leetcode.com/problems/detect-capital/
+ */
+
+var detectCapitalUse = function (word) {
+    if (word.toUpperCase() === word) {
+        return true;
+    }
+
+    if (word.toLowerCase() === word) {
+        return true;
+    }
+
+    if (word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase() === word) {
+        return true;
+    }
+
+    return false;
+};
