54eacb0811c300e93af6c97988c4e88dcc7b9121
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-10-08 20:01:09 +0300

    Smallest Even Multiple

---
+++ easy/Smallest Even Multiple.js
@@ -0,0 +1,9 @@
+/*
+Smallest Even Multiple
+
+https://leetcode.com/problems/smallest-even-multiple/
+ */
+
+const smallestEvenMultiple = n => n % 2 === 0 ? n : n * 2;
+
+console.log(smallestEvenMultiple(5))
