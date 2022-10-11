45c0a2dae5218a97fb1c6bb78725f80ad5e75a4d
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-10-12 00:45:02 +0300

    Smallest Index With Equal Value

---
+++ medium/Find the Duplicate Number.js
@@ -0,0 +1,13 @@
+/*
+Find the Duplicate Number
+
+https://leetcode.com/problems/find-the-duplicate-number/
+ */
+
+var findDuplicate = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) return nums[i]
+        }
+    }
+};
