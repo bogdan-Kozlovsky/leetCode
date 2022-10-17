80b1f01819526d1961ef8c1462bab3d96edf8dbf
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-10-17 20:52:01 +0300

    Binary Search

---
+++ easy/Binary Search.js
@@ -0,0 +1,11 @@
+/*
+Binary Search
+
+https://leetcode.com/problems/binary-search/?envType=study-plan&id=algorithm-i
+ */
+
+var search = function (nums, target) {
+    return nums.indexOf(target);
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
